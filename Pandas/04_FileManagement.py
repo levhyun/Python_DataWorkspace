e68633a5a246f54fe6d1b167714ade7874b364dc
levhyun <g88080997@gmail.com>
2022-10-30 11:23:26 +0900

    data

--- Pandas/04_FileManagement.py
+++ Pandas/04_FileManagement.py
@@ -17,20 +17,41 @@ df = pd.DataFrame(data, index=['no.1','no.2','no.3','no.4','no.5','no.6','no.7',
 df.index.name = '지원번호'
 print(df)
 
-# 저장하기
+# 저장
 
-# csv파일로 저장
+# csv 파일로 저장
 df.to_csv('score.csv', encoding='utf-8-sig')
 df.to_csv('not_index_score.csv', encoding='utf-8-sig', index=False) # index없이 저장
 
-# txt파일로 저장
-df.to_csv('score .txt', sep='\t') # tab으로 구분된 텍스트파일
+# txt 파일로 저장
+df.to_csv('score.txt', sep='\t') # tab으로 구분된 텍스트파일
+
+# 엑셀 파일로 저장
+df.to_excel('score.xlsx')
 
 
+# 열기
 
+# csv 파일 열기
+df = pd.read_csv('score.csv')
+print('\n\n[read_csv]\n\n',df)
 
+df = pd.read_csv('score.csv', skiprows=1)
+print('\n\n[read_csv] - 첫 번째 줄 무시 출력\n\n',df)
 
+df = pd.read_csv('score.csv', skiprows=[2,4,6])
+print('\n\n[read_csv] - 2,4,6 번째 줄 무시 출력\n\n',df)
 
+df = pd.read_csv('score.csv', nrows=2)
+print('\n\n[read_csv] - 2번째 줄 까지만 출력\n\n',df)
+
+# txt 파일 열기
+df = pd.read_csv('score.txt', sep='\t')
+print('\n\n[txt]\n\n',df)
+
+# 엑셀 파일 열기
+df = pd.read_excel('score.xlsx')
+print('\n\n[excel]\n\n',df)
 
 
 
